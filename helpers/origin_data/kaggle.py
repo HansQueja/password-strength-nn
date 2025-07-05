516e3ac58a16b29cb66941382ce1f742085e7176
Hans Queja <hansqueja8@gmail.com>
2025-07-06 00:51:50 +0800

    Add kaggle script

---
+++ helpers/origin_data/kaggle.py
@@ -0,0 +1,11 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("bhavikbb/password-strength-classifier-dataset")
+
+print("Path to dataset files:", path)
+
+# Download latest version
+path = kagglehub.dataset_download("wjburns/common-password-list-rockyoutxt")
+
+print("Path to dataset files:", path)
